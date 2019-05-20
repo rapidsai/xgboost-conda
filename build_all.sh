46bfe757074c2fab9358aa0404afe5feb04152fd
Raymond Douglass <ray@raydouglass.com>
2019-05-20 15:17:04 -0400

    BLD Remove driver install

--- build_all.sh
+++ build_all.sh
@@ -2,17 +2,6 @@
 
 export PATH=/conda/bin:/usr/local/cuda/bin:$PATH
 
-apt-get update -q
-DRIVER_VER="396.44-1"
-LIBCUDA_VER="396"
-if [ "$CUDA" == "10.0" ]; then
-  DRIVER_VER="410.72-1"
-  LIBCUDA_VER="410"
-fi
-DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
-  cuda-drivers=${DRIVER_VER} libcuda1-${LIBCUDA_VER}
-
-
 source activate gdf
 
 conda build -c rapidsai-nightly -c rapidsai-nightly/label/xgboost -c rapidsai -c rapidsai/label/xgboost -c nvidia -c conda-forge -c defaults \

b15cfbacad264a68e234bd38afa38b22cd4de5a3
Raymond Douglass <ray@raydouglass.com>
2019-06-19 15:17:18 -0400

    Specify python version to conda-build

--- build_all.sh
+++ build_all.sh
@@ -8,9 +8,9 @@ source activate gdf
 # conda build -c conda-forge -c defaults recipes/nvcc
 # conda build -c conda-forge -c defaults recipes/nccl
 
-conda build -c rapidsai -c rapidsai/label/xgboost -c nvidia -c conda-forge -c defaults \
+conda build -c rapidsai -c rapidsai/label/xgboost -c nvidia -c conda-forge -c defaults --python=$PYTHON  \
     recipes/xgboost recipes/dask-xgboost
 
-conda build -c rapidsai -c rapidsai/label/xgboost -c nvidia -c conda-forge -c defaults \
+conda build -c rapidsai -c rapidsai/label/xgboost -c nvidia -c conda-forge -c defaults --python=$PYTHON \
     recipes/xgboost recipes/dask-xgboost --output | xargs \
     anaconda -t ${MY_UPLOAD_KEY} upload -u ${CONDA_USERNAME:-rapidsai-nightly} --label xgboost --force

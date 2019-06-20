abc84c7505e91b742b1a8eca254366b333af4a68
Raymond Douglass <ray@raydouglass.com>
2019-06-20 11:25:11 -0400

    Fix typo

--- build_all.sh
+++ build_all.sh
@@ -9,14 +9,18 @@ conda build -c conda-forge -c defaults recipes/nvcc
 conda build -c conda-forge -c defaults recipes/nvcc --output | xargs \
   anaconda -t ${NVIDIA_UPLOAD_KEY} upload -u ${NVIDIA_CONDA_USERNAME:-nvidia} --force
 
-conda build -c ${NVIDIA_CONDA_USERNAME}:-nvidia} -c conda-forge -c defaults recipes/nccl
-conda build -c ${NVIDIA_CONDA_USERNAME}:-nvidia} -c conda-forge -c defaults recipes/nccl --output | xargs \
+conda build -c ${NVIDIA_CONDA_USERNAME:-nvidia} -c conda-forge -c defaults recipes/nccl
+conda build -c ${NVIDIA_CONDA_USERNAME:-nvidia} -c conda-forge -c defaults recipes/nccl --output | xargs \
   anaconda -t ${NVIDIA_UPLOAD_KEY} upload -u ${NVIDIA_CONDA_USERNAME:-nvidia} --force
 
 
-conda build -c ${CONDA_USERNAME:-rapidsai} -c ${NVIDIA_CONDA_USERNAME}:-nvidia} -c conda-forge -c defaults --python=$PYTHON  \
+conda build -c ${CONDA_USERNAME:-rapidsai} -c ${NVIDIA_CONDA_USERNAME:-nvidia} -c conda-forge -c defaults --python=$PYTHON  \
     recipes/xgboost recipes/dask-xgboost
 
-conda build -c ${CONDA_USERNAME:-rapidsai} -c ${NVIDIA_CONDA_USERNAME}:-nvidia} -c conda-forge -c defaults --python=$PYTHON  \
+conda build -c ${CONDA_USERNAME:-rapidsai} -c ${NVIDIA_CONDA_USERNAME:-nvidia} -c conda-forge -c defaults --python=$PYTHON  \
     recipes/xgboost recipes/dask-xgboost --output | xargs \
     anaconda -t ${MY_UPLOAD_KEY} upload -u ${CONDA_USERNAME:-rapidsai} --label xgboost --force
+
+
+    conda build -c /conda/envs/gdf/conda-bld/ -c ${CONDA_USERNAME:-rapidsai} -c ${NVIDIA_CONDA_USERNAME}:-nvidia} -c conda-forge -c defaults --python=$PYTHON  \
+        recipes/xgboost recipes/dask-xgboost

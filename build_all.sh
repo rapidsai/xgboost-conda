dc6e7b2ce59d824df1af17c36a3741dfb467e85e
Raymond Douglass <ray@raydouglass.com>
2019-09-30 12:26:11 -0400

    Remove nvcc/nccl and use xgboost version

--- build_all.sh
+++ build_all.sh
@@ -3,16 +3,10 @@
 export PATH=/conda/bin:/usr/local/cuda/bin:$PATH
 export HOME=$WORKSPACE
 
+version=`curl -s https://raw.githubusercontent.com/dmlc/xgboost/master/python-package/xgboost/VERSION`
+export XGBOOST_VERSION=${version/-/.}
+
 source activate gdf
-
-conda build -c conda-forge -c defaults recipes/nvcc
-conda build -c conda-forge -c defaults recipes/nvcc --output | xargs \
-  anaconda -t ${NVIDIA_UPLOAD_KEY} upload -u ${NVIDIA_CONDA_USERNAME:-nvidia} --force
-
-conda build -c ${NVIDIA_CONDA_USERNAME:-nvidia} -c conda-forge -c defaults recipes/nccl
-conda build -c ${NVIDIA_CONDA_USERNAME:-nvidia} -c conda-forge -c defaults recipes/nccl --output | xargs \
-  anaconda -t ${NVIDIA_UPLOAD_KEY} upload -u ${NVIDIA_CONDA_USERNAME:-nvidia} --force
-
 
 conda build -c ${CONDA_USERNAME:-rapidsai} -c ${NVIDIA_CONDA_USERNAME:-nvidia} -c conda-forge -c defaults --python=$PYTHON  \
     recipes/xgboost recipes/dask-xgboost

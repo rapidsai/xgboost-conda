c48f7093c84cf8f6e890ad092650aeaff6e6bd21
Ray Douglass <3107146+raydouglass@users.noreply.github.com>
2019-10-15 19:13:14 -0400

    Update build_all.sh

--- build_all.sh
+++ build_all.sh
@@ -15,4 +15,4 @@ conda build -c ${CONDA_USERNAME:-rapidsai} -c ${NVIDIA_CONDA_USERNAME:-nvidia} -
 
 conda build -c ${CONDA_USERNAME:-rapidsai} -c ${NVIDIA_CONDA_USERNAME:-nvidia} -c conda-forge -c defaults --python=$PYTHON  \
     recipes/xgboost recipes/dask-xgboost --output | xargs \
-    anaconda -t ${MY_UPLOAD_KEY} upload -u ${CONDA_USERNAME:-rapidsai} --label xgboost --force
+    anaconda -t ${MY_UPLOAD_KEY} upload -u ${CONDA_USERNAME:-rapidsai} --label main --force

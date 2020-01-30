71bfb0353dab10a66128b52324050a9ada722f64
Mike Wendt <mike.wendt@me.com>
2020-01-30 02:24:34 -0500

    FIX Skip nvcc/nccl builds

--- build_all.sh
+++ build_all.sh
@@ -9,12 +9,13 @@ if [ -z "$XGBOOST_VERSION" ]; then
 fi
 
 source activate gdf
-conda build -c conda-forge -c defaults recipes/nvcc
-conda build -c ${NVIDIA_CONDA_USERNAME:-nvidia} -c conda-forge -c defaults recipes/nccl
+#conda build -c conda-forge -c defaults recipes/nvcc
+#conda build -c ${NVIDIA_CONDA_USERNAME:-nvidia} -c conda-forge -c defaults recipes/nccl
 
 conda build -c ${CONDA_USERNAME:-rapidsai} -c ${NVIDIA_CONDA_USERNAME:-nvidia} -c conda-forge -c defaults --python=$PYTHON  \
     recipes/xgboost recipes/dask-xgboost
 
 conda build -c ${CONDA_USERNAME:-rapidsai} -c ${NVIDIA_CONDA_USERNAME:-nvidia} -c conda-forge -c defaults --python=$PYTHON  \
     recipes/xgboost recipes/dask-xgboost --output | xargs \
-    anaconda -t ${MY_UPLOAD_KEY} upload -u ${CONDA_USERNAME:-rapidsai} --label main --force
+
+anaconda -t ${MY_UPLOAD_KEY} upload -u ${CONDA_USERNAME:-rapidsai} --label main --force

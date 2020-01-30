743adbf010b9cd0a7e5c748f5e8a537c9ef51827
Mike Wendt <mike.wendt@me.com>
2020-01-30 03:26:38 -0500

    FIX Correct xargs command

--- build_all.sh
+++ build_all.sh
@@ -17,5 +17,4 @@ conda build -c ${CONDA_USERNAME:-rapidsai} -c ${NVIDIA_CONDA_USERNAME:-nvidia} -
 
 conda build -c ${CONDA_USERNAME:-rapidsai} -c ${NVIDIA_CONDA_USERNAME:-nvidia} -c conda-forge -c defaults --python=$PYTHON  \
     recipes/xgboost recipes/dask-xgboost --output | xargs \
-
-anaconda -t ${MY_UPLOAD_KEY} upload -u ${CONDA_USERNAME:-rapidsai} --label main --force
+    anaconda -t ${MY_UPLOAD_KEY} upload -u ${CONDA_USERNAME:-rapidsai} --label main --force

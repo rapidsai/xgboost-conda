fac0c66bed9304153b356ab009dfd4013b8eec13
Ray Douglass <3107146+raydouglass@users.noreply.github.com>
2020-10-19 13:50:25 -0400

    Don't try to copy static libs

--- recipes/xgboost/install-libxgboost.sh
+++ recipes/xgboost/install-libxgboost.sh
@@ -27,7 +27,6 @@ else
 fi
 
 mkdir -p ${LIBDIR} ${INCDIR}/xgboost ${BINDIR} || true
-cp -f ${SRC_DIR}/lib/*.a ${LIBDIR}/
 cp ${SRC_DIR}/xgboost${EXEEXT} ${BINDIR}/
 cp ${SRC_DIR}/lib/${XGBOOSTDSO} ${SODIR}/
 cp -Rf ${SRC_DIR}/include/xgboost ${INCDIR}/
